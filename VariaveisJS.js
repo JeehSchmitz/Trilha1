e15777341873f8cdc76f41b815200f7be4c29441
JeehSchmitz <94876128+JeehSchmitz@users.noreply.github.com>
2023-06-27 18:29:41 -0300

    Add files via upload

---
+++ VariaveisJS.js
@@ -0,0 +1,13 @@
+var pagina = "A página foi carregada com sucesso!";
+alert(pagina);
+
+var MinhaIdade=31;
+var IdadeMarido=32;
+
+document.write("A soma das nossas idades é: " +(MinhaIdade+IdadeMarido));
+document.write("<br>");
+document.write("Minha idade dividida por 2 é: " +(MinhaIdade/2));
+document.write("<br>");
+document.write("Sua idade multiplicada por 8 é: " +(IdadeMarido*8));
+document.write("<br>");
+document.write("Nossas idades são iguais? " +(MinhaIdade==IdadeMarido));
